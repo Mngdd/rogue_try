a15d0949eb6683be90844db21a1e6a9f22ea86e3
Mngdd <74361463+Mngdd@users.noreply.github.com>
2024-08-09 02:09:37 +0300

    hud base ready, death/heal triggers ready

--- Code/Player/Player.cs
+++ Code/Player/Player.cs
@@ -7,26 +7,32 @@ public partial class Player : CharacterBody2D {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
 	private bool _debugEnabled = false;
-
+	private float _hpSizeX = 0;
 	[Export] public int MaxHealth = 100;
-	[Export] public int Health = 100;
+	[Export] public int CurrentHealth = 100;
 
 	private bool _dead = false;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	
+
+	public override void _Ready() {
+		_hpSizeX = GetNode<CanvasLayer>("HUD").GetNode<TextureRect>("HealthBar").Size.X;
+	}
+
 	// обработка добавления хп
 	public void AddHealth(int amount) {
-		int newHealth = amount + Health;
+		int newHealth = amount + CurrentHealth;
+		GD.Print("amount: " + amount + ", new: " + newHealth);
+
 		if (newHealth > MaxHealth) {
-			Health = MaxHealth;
+			CurrentHealth = MaxHealth;
 		} else if (newHealth <= 0) {
-			Health = 0;
+			CurrentHealth = 0;
 			_dead = true;
 		} else {
-			Health = newHealth;
+			CurrentHealth = newHealth;
 		}
 	}
 
@@ -38,8 +44,9 @@ public partial class Player : CharacterBody2D {
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
 
+		// user-controlled movement inside (if alive!)
 		// Handle Jump.
-		if (Input.IsActionJustPressed("move_up") && IsOnFloor())
+		if (Input.IsActionJustPressed("move_up") && IsOnFloor() && !_dead)
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
@@ -47,27 +54,36 @@ public partial class Player : CharacterBody2D {
 		Vector2 direction = Input.GetVector(
 			"move_left", "move_right", "move_up", "move_down"
 		);
-		if (direction != Vector2.Zero) {
+		if (direction != Vector2.Zero && !_dead) {
 			velocity.X = direction.X * Speed;
 		} else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
 		Velocity = velocity;
-		MoveAndSlide(); // обновляет велосити как я понял...
-		//
-		// if (Position.Y > 10000)  // OUT OF BOUNDS, KILL
-		// {
-		// 	// TODO: kill player here!
-		// }
+		MoveAndSlide(); // player update
+
+		if (Position.Y > 10000) // OUT OF BOUNDS, KILL
+		{
+			AddHealth(-MaxHealth);
+			// maybe freeze body when y >10k && _dead
+		}
 	}
 
 	public override void _Process(double delta) {
-		GetNode<CanvasLayer>("HUD").GetNode<Label>("debug").Text =
+		var hud = GetNode<CanvasLayer>("HUD");
+		string aliveOrNot = _dead ? "DEAD" : "ALIVE";
+		hud.GetNode<Label>("debug").Text =
 			@$"DEBUG INFO:
 			POS X: {Position.X}
 			POS Y: {Position.Y}
 			VEL X: {Velocity.X}
-			VEL Y: {Velocity.Y}";
+			VEL Y: {Velocity.Y}
+			----
+			STATUS: {aliveOrNot}
+			HP: {CurrentHealth}";
+
+		var hpBar = hud.GetNode<TextureRect>("HealthBar");
+		hpBar.Size = new Vector2(_hpSizeX * ((float)CurrentHealth / MaxHealth), hpBar.Size.Y);
 	}
 }
--- Code/Tools/ChangeHealthTrigger.cs
+++ Code/Tools/ChangeHealthTrigger.cs
@@ -2,11 +2,12 @@ using Godot;
 using System;
 
 public partial class ChangeHealthTrigger : Node2D {
+	
 	private bool _canDamage = false;
-	[Export] public int HealthAdd = 10;
+	[Export] public int HealthAdd = 0;
 	[Export] public bool PushBack = false;  //TODO: IMPLEMENT ME
-	[Export] public double ActInterval = 1d;
-
+	[Export] public double ActInterval = 2d;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 	}
@@ -14,40 +15,50 @@ public partial class ChangeHealthTrigger : Node2D {
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
 	}
-	//TODO: FIX ENTER-ACT-TIMOUT-EXIT DAMAGE SEQUENCE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	
+	// костыльно, таймеры (возможно они вредны но я хз)
 	private void _on_area_2d_body_entered(Node2D body) {
-		// Replace with function body.
 		//if (body is CharacterBody2D) // можно и другие сравнения...
 		if (body is Player) {
 			_canDamage = true;
 			GD.Print("ENTITY ENTERED: " + body);
-			
-			var timer = GetNode<Timer>("Cooldown");
-			timer.Connect("timeout", new Callable(this, MethodName._act));
-		}
-	}
 
-	private void _act(Node2D body) {
-		Player pl = (Player)body;
-		pl.AddHealth(HealthAdd);
+			Callable callable = Callable.From(() => _act(body));
+
+			var timer = GetNode<Timer>("Cooldown");
+			timer.Connect("timeout", callable);
+			timer.WaitTime = ActInterval;
+			timer.Start();
+		}
 	}
 	
 	private void _on_area_2d_body_exited(Node2D body) {
 		if (body is Player) {
 			_canDamage = false;
 			GD.Print("ENTITY EXITED: " + body);
-			GetNode<Timer>("Cooldown").Stop();
-			GetNode<Timer>("Cooldown").WaitTime = ActInterval;
+			
+			var timer = GetNode<Timer>("Cooldown");
+			timer.Stop();
+			timer.WaitTime = ActInterval;
 		}
-		// Replace with function body.
 	}
-
+	
+	private void _act(Node2D body) {
+		if (!_canDamage)
+			return;
+		
+		GD.Print("_act, does damage stuff lolll");
+		Player pl = (Player)body;
+		pl.AddHealth(HealthAdd);
+		// then repeat...
+	}
+	
+	// obsolete rn
 	private void _on_cooldown_timeout() {
-		if (_canDamage) {
-			GD.Print("DOIN_DAMAGE");
-			GetNode<Timer>("Cooldown").Start();
-		} else {
-			GD.Print("exited trigga");
-		}
+		// if (_canDamage) {
+		// 	EmitSignal(SignalName.InteractWithEntity);
+		// } else {
+		// 	GD.Print("exited trigga");
+		// }
 	}
 }
